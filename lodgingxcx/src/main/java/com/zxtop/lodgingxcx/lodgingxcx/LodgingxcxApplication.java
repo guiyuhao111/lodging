60962255e4cce0098cc04f05e00331c1c61c1047
guiyuhao111 <1060455912@qq.com>
2018-09-14 18:18:02 +0800

    第一次提交

---
+++ lodgingxcx/src/main/java/com/zxtop/lodgingxcx/lodgingxcx/LodgingxcxApplication.java
@@ -0,0 +1,12 @@
+package com.zxtop.lodgingxcx.lodgingxcx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgingxcxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LodgingxcxApplication.class, args);
+    }
+}
